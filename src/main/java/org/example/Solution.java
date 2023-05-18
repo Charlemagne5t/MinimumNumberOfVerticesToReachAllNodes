4b8c9512b161a68b5a2e9efe63f744fd33d377ad
alek <wait82985@gmail.com>
2023-05-18 13:01:23 +0600

    problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,10 +1,20 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 public class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        return new ArrayList<>();
+        Set<Integer> setOfNodes = new HashSet<>();
+
+        for (int i = 0; i < edges.size(); i++) {
+            setOfNodes.add(edges.get(i).get(0));
+        }
+
+        for (int i = 0; i < edges.size(); i++) {
+            setOfNodes.remove(edges.get(i).get(1));
+
+        }
+        return new ArrayList<>(setOfNodes);
     }
 }
